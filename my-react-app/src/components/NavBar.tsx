ae48069263f4e7cc5e87f039034aa994b3cafcf8
manon labat <manonlabat10@gmail.com>
2024-10-26 14:05:55 +0200

    renderinglist1

--- my-react-app/src/components/NavBar.tsx
+++ my-react-app/src/components/NavBar.tsx
@@ -13,24 +13,18 @@ interface Pokemon {
   function NavBar({ pokemonIndex, setPokemonIndex, pokemonList}: NavBarProps) {
 
     
-    const handleClick = () => {
-    if (pokemonIndex < pokemonList.length -1){
-      setPokemonIndex (pokemonIndex +1 );
-    }
-  }
-  const handleOffClick = () => {
-    if (pokemonIndex > 0){
-      setPokemonIndex (pokemonIndex -1)
-    }
-  }
+    
   return (
     <>
      <div>
       <PokemonCard pokemon={pokemonList[pokemonIndex]} />
-      {pokemonIndex < pokemonList.length -1 &&(
-      <button onClick={handleClick}>Précedent</button>)}
-      {pokemonIndex > 0 &&  (
-      <button onClick={handleOffClick}>Suivant</button>)}
+        {pokemonList.map((Pokemon, index)=>(
+            <button
+            key={index}
+            onClick={()=> setPokemonIndex (index)}>
+        {Pokemon.name}
+        </button>        
+        ))}
     </div>
     </>
   );
